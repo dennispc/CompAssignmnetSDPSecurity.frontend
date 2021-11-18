671edc3fd855f8ced145bd9b880efdc844198605
jbn108 <jbuus9@gmail.com>
2021-11-18 11:50:36 +0100

    auth added

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'products', loadChildren: () =>
+  {
+    path: 'products', loadChildren: () =>
     import('./products/products.module')
-      .then(f => f.ProductsModule)}
+      .then(f => f.ProductsModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module')
+      .then(m => m.AuthModule)
+  },
 ];
 
 @NgModule({
